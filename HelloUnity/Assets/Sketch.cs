9f0151c7813a70ea1c3453f78a4417ad379dca11
James Chen <jche846@aucklanduni.ac.nz>
2016-10-07 14:16:52 +1300

    connected to azure
    connected to azure

--- HelloUnity/Assets/Sketch.cs
+++ HelloUnity/Assets/Sketch.cs
@@ -3,7 +3,7 @@ using Pathfinding.Serialization.JsonFx; //make sure you include this using
 
 public class Sketch : MonoBehaviour {
     public GameObject myPrefab;
-	public string _WebsiteURL = "http://infosys320lab2.azurewebsites.net/tables/product?zumo-api-version=2.0.0";
+	private string _WebsiteURL = "http://infosys320lab2.azurewebsites.net/tables/TrelloData?zumo-api-version=2.0.0";
 
     void Start () {
         //Reguest.GET can be called passing in your ODATA url as a string in the form:
@@ -18,14 +18,14 @@ public class Sketch : MonoBehaviour {
         }
 
         //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
-        Product[] products = JsonReader.Deserialize<Product[]>(jsonResponse);
+		Trello[] cards = JsonReader.Deserialize<Trello[]>(jsonResponse);
 
-		int totalCubes = products.Length;
+		int totalCubes = cards.Length;
 		int totalDistance = 5;
 		int i = 0;
 
         //We can now loop through the array of objects and access each object individually
-        foreach (Product product in products)
+		foreach (Trello item in cards)
         {
 			float perc = i / (float)totalCubes;
 			i++;
@@ -35,7 +35,7 @@ public class Sketch : MonoBehaviour {
 			GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
 			newCube.GetComponent<CubeScript>().SetSize((1.0f - perc) * 2);
 			newCube.GetComponent<CubeScript>().rotateSpeed = perc;
-			newCube.GetComponentInChildren<TextMesh>().text = product.ProductName;
+			newCube.GetComponentInChildren<TextMesh>().text = item.Owner;
         }
 	}
 	
