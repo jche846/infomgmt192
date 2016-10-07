bc27acac096c4e05576c96cb0e3957b53088c3d2
James Chen <jche846@aucklanduni.ac.nz>
2016-10-07 13:56:25 +1300

    finished trello class
    class

--- HelloUnity/Assets/Trello.cs
+++ HelloUnity/Assets/Trello.cs
@@ -6,6 +6,9 @@ using System.Text;
 class Trello {
 
 	//List all the columns of your table here in the same format as below.
+	public string Operation { get; set; }
+	public string Date { get; set; }
+	public string Owner { get; set; }
 
 
 }
